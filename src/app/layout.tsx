f412cb8d5d48b8ea19db7bad0757eefdd0e67547
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-02-28 11:11:03 -0500

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Xin Wen Yap - Full Stack Developer',
+  title: 'Xin Wen Yap',
   description: '',
   metadataBase: new URL('https://example.com'),
   robots: 'index, follow',
